cd887f52385b0a0ece491faa6eeb828ab860e4bc
xutao-wang <xutao.wang16@gmail.com>
2020-01-12 22:05:31 -0500

    Initial Commit
    Former-commit-id: a9aee67638853c617edc062bb10a6fa9496a05f6
    Former-commit-id: 1a432bbfb64198a361c2c81e3455cc4da3176729 [formerly 2827e06622a47d7d1e044bb1686c8554216c0ab4] [formerly c916445b5b4916ce7dbc5f883a364e4367ace5af [formerly ee9d17fb80c5e11551211fbbe77e73f59c39b000]]
    Former-commit-id: 7532a593918de2406520e6dbf1773da7c992378d [formerly 5955ae5bd4a1d72f4867ec7f512f7f65faff1e87]
    Former-commit-id: 96fe1739c04674d131ce128f73a1b4e1d3b66316

--- R/data.R
+++ R/data.R
@@ -4,3 +4,24 @@
 #' of signatures uses gene symbols. Attempts have been made to use updated gene
 #' symbols and remove symbols that did not match the most recent annotation.
 #' Additional sets for Entrez IDs and Ensembl IDs are forthcoming.
+#'
+#' Signature names are composed of the last name of the primary author,
+#' followed by
+#' a possible context for the signature, and ending with either the number of
+#' gene transcripts or genes in the signature, with respect to however
+#' it was described in the signature in the original publication.
+#'
+#' Possible signature contexts:
+#' \itemize{
+#' \item{OD: Other diseases}
+#' \item{HIV: Human Immunodeficiency Virus}
+#' \item{PNA: Pneumonia}
+#' \item{RISK: Risk of developing active TB}
+#' \item{RES: Response to TB treatment}
+#' \item{FAIL: Failure of TB treatment}
+#' }
+#'
+#' Note that in some cases signatures will be positive identifiers of TB
+#' whereas others are negative identifiers; this should be taken into account
+#' when creating ROC curves and computing any AUC estimates.
+#'
