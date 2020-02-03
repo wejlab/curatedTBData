4048da4d4c53688e205eb56817161330ff1e2fc5
xutao-wang <xutao.wang16@gmail.com>
2020-02-02 20:14:48 -0500

    Add new data

--- R/CreateMultiAssayObject.R
+++ R/CreateMultiAssayObject.R
@@ -36,7 +36,13 @@ Create_MultiAssay_object <- function(sobject){
     # Add new column to the expression matrix
     sobject_exprs_new <- sobject_exprs %>% as_tibble() %>% mutate(SYMBOL=row_data$SYMBOL_NEW) %>% filter(SYMBOL!='NA')
 
-    ## Create expression matrix with gene symbol, take mean values for same genes
+    # Expand probe sets for non-specific probes if apllicable
+    if(!identical(grep("///",sobject_exprs_new$SYMBOL), integer(0))){
+      sobject_exprs_new <- expandProbesets(sobject_exprs_new, sep = "///")
+
+    }
+
+    # Create expression matrix with gene symbol, take mean values for same genes
     sobject_exprs_symbol <- sobject_exprs_new %>% group_by(SYMBOL) %>% summarise_all(mean) %>% data.frame()
     row.names(sobject_exprs_symbol) <- sobject_exprs_symbol$SYMBOL
 
@@ -58,7 +64,40 @@ Create_MultiAssay_object <- function(sobject){
 
     return(simpleMultiAssay)
 
+}
 
+#' Expand probe set for non-specific probes
+#' @name expandProbesets
+#'
+#' @param dat_example A matrix with one column represents gene symbol with column name `SYMBOL`, and rest of columns are gene expression value from each sample
+#' @return A matrix that split non-uniquely mapped features to one per row
+#' @examples
+#' dat_example <- data.frame(SYMBOL=c("OR7E14P///OR7E12P","CEP104///LILRA6",
+#' "SNAR-A1///SNAR-A2///SNAR-A12","ALG6"),sample1=rnorm(4),sample2=rnorm(4))
+#' expandProbesets(dat_example, sep="///")
+#' @export
+expandProbesets <- function(sobject_exprs_new, sep="///"){
 
+  # Get index with duplicated symbol
+  index <- grep("///",sobject_exprs_new$SYMBOL)
+  sobject_exprs_dup <- sobject_exprs_new[index,]
+
+  x_list <- strsplit(as.character(sobject_exprs_dup$SYMBOL), sep)
+  symbol_dup <- gsub(" ","",unlist(x_list))
+  sobject_exprs_dup$times <- sapply(x_list, length)
+
+  # Expand rows based on their frequency
+  sobject_exprs_expand <- as.data.frame(lapply(sobject_exprs_dup, rep, sobject_exprs_dup$times))
+  sobject_exprs_expand$SYMBOL <- symbol_dup
+  sobject_exprs_expand_result <- sobject_exprs_expand %>% dplyr::select(-times)
+
+  # double-check SYMBOL does not contain NA
+  if(any(is.na(sobject_exprs_expand_result$SYMBOL))){
+    stop("Expand probe sets contain NA's, please check")
+  }
+
+  sobject_exprs_result <- rbind(sobject_exprs_new[-index,],sobject_exprs_expand_result)
+  return(sobject_exprs_result)
 
 }
+
