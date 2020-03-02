e4257ca6eb2752270cc6562414222b67285d641b
xutao-wang <xutao.wang16@gmail.com>
2020-03-02 00:14:22 -0500

    Modify data/functions

--- R/CreateMultiAssayObject.R
+++ R/CreateMultiAssayObject.R
@@ -1,4 +1,4 @@
-#' Create MultiAssay Experiment Object from Summarized Experiment Object.
+#' Expand MultiAssay Experiment Object from SummarizedExperiment/MultiAssayExperiment Object.
 #' @name Create_MultiAssay_object
 #'
 #' @param sobject A summarized experiment object
@@ -13,25 +13,25 @@ Create_MultiAssay_object <- function(sobject){
   if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
   if (! "MultiAssayExperiment" %in% installed.packages()) BiocManager::install("MultiAssayExperiment")
-  if(class(sobject) != "SummarizedExperiment"){
-    stop(paste("Invalid input data type. Only supported for SummarizedExperiment objects. Your input:,", typeof(sobject)))
+  if(!any(class(sobject1) == c("SummarizedExperiment","MultiAssayExperiment"))){
+    stop(paste("Invalid input data type. Only supported for SummarizedExperiment/MultiAssayExperiment objects. Your input:,", typeof(sobject)))
   }
+  library(SummarizedExperiment)
   library(MultiAssayExperiment)
   library(dplyr)
-  # Read in Data
-  sobject_exprs <- assay(sobject)
-  row_data <- rowData(sobject) %>% data.frame()
-  if (!any(colnames(row_data)=="SYMBOL_NEW")){
-    stop("RowData of the input Summarized Experiment Object does not have SYMBOL_NEW column, add SYMBOL_NEW column that includes gene symbols.")
-  }
-  if (!any(colnames(row_data)=="ID_REF")){
-    stop("RowData of the input Summarized Experiment Object does not have ID_REF column, add ID_REF column that includes probe IDs.")
-  }
-  if (!all(row.names(assay(sobject))==row_data$ID_REF)){
-    stop("Input Summarized Experiment Object row names are not exactly the same with ID_REF from row Data, consider change")
-  }
-
-  ## Starting create Multi-assay experiment
+  if (class(sobject) == "SummarizedExperiment"){
+    # read in data
+    sobject_exprs <- assay(sobject)
+    row_data <- rowData(sobject) %>% data.frame()
+    if (!any(colnames(row_data)=="SYMBOL_NEW")){
+      stop("RowData of the input Summarized Experiment Object does not have SYMBOL_NEW column, add SYMBOL_NEW column that includes gene symbols.")
+    }
+    if (!any(colnames(row_data)=="ID_REF")){
+      stop("RowData of the input Summarized Experiment Object does not have ID_REF column, add ID_REF column that includes probe IDs.")
+    }
+    if (!all(row.names(assay(sobject))==row_data$ID_REF)){
+      stop("Input Summarized Experiment Object row names are not exactly the same with ID_REF from row Data, consider change")
+    }
 
     # Add new column to the expression matrix
     sobject_exprs_new <- sobject_exprs %>% as_tibble() %>% mutate(SYMBOL=row_data$SYMBOL_NEW) %>% filter(SYMBOL!='NA')
@@ -39,30 +39,57 @@ Create_MultiAssay_object <- function(sobject){
     # Expand probe sets for non-specific probes if apllicable
     if(!identical(grep("///",sobject_exprs_new$SYMBOL), integer(0))){
       sobject_exprs_new <- expandProbesets(sobject_exprs_new, sep = "///")
-
     }
 
     # Create expression matrix with gene symbol, take mean values for same genes
     sobject_exprs_symbol <- sobject_exprs_new %>% group_by(SYMBOL) %>% summarise_all(mean) %>% data.frame()
     row.names(sobject_exprs_symbol) <- sobject_exprs_symbol$SYMBOL
 
-    sobject_exprs_symbol <- sobject_exprs_symbol[,-which(colnames(sobject_exprs_symbol) %in% 'SYMBOL')]
+    sobject_exprs_symbol <- sobject_exprs_symbol[,-which(colnames(sobject_exprs_symbol) %in% 'SYMBOL')] %>% as.matrix
 
-    ## Create MultiAssayExperiment object
-    doubleExp <- list('Probe' = sobject, 'Symbol' = sobject_exprs_symbol)
-    symbol_map <- data.frame(primary = row.names(colData(sobject)),
-                             colname = row.names(colData(sobject)), stringsAsFactors = FALSE)
-    probe_map <- data.frame(primary_info = rep("n.a.",length(row.names(colData(sobject)))),stringsAsFactors = FALSE)
-    row.names(probe_map) <- row.names(colData(sobject))
+    ## Create MultiAssayExperiment object Use methods from CreateSobject
+    mobject1 <- new("Mobject", assay_reprocess = sobject_exprs_symbol, assay_raw = sobject@assays[[1]],
+                    row_data = data.frame(sobject@elementMetadata), primary = data.frame(sobject@colData),
+                    meta_data = sobject@metadata[[1]])
 
-    listmap <- list(symbol_map,symbol_map)
-    names(listmap) <- c("Probe", "Symbol")
-    dfmap <- listToMap(listmap)
-
-    simpleMultiAssay <- MultiAssayExperiment(experiments=doubleExp,
-                                             colData=probe_map,dfmap)
-
+    simpleMultiAssay <- CreateObject(mobject1,assay_type = "assay_reduce")
     return(simpleMultiAssay)
+  }
+
+  if (class(sobject) == "MultiAssayExperiment"){
+    sobject_ori <- MultiAssayExperiment::experiments(sobject)[["assay_raw"]]
+    sobject_exprs <- assay(sobject_ori)
+    row_data <- rowData(sobject_ori) %>% data.frame()
+    if (!any(colnames(row_data)=="SYMBOL_NEW")){
+      stop("RowData of the input Summarized Experiment Object does not have SYMBOL_NEW column, add SYMBOL_NEW column that includes gene symbols.")
+    }
+    if (!any(colnames(row_data)=="ID_REF")){
+      stop("RowData of the input Summarized Experiment Object does not have ID_REF column, add ID_REF column that includes probe IDs.")
+    }
+    if (!all(row.names(assay(sobject_ori))==row_data$ID_REF)){
+      stop("Input Summarized Experiment Object row names are not exactly the same with ID_REF from row Data, consider change")
+    }
+
+    ## Starting create Multi-assay experiment
+
+    # Add new column to the expression matrix
+    sobject_exprs_new <- sobject_exprs %>% as_tibble() %>% mutate(SYMBOL=row_data$SYMBOL_NEW) %>% filter(SYMBOL!='NA')
+
+    # Expand probe sets for non-specific probes if apllicable
+    if(!identical(grep("///",sobject_exprs_new$SYMBOL), integer(0))){
+      sobject_exprs_new <- expandProbesets(sobject_exprs_new, sep = "///")
+    }
+
+    # Create expression matrix with gene symbol, take mean values for same genes
+    sobject_exprs_symbol <- sobject_exprs_new %>% group_by(SYMBOL) %>% summarise_all(mean) %>% data.frame()
+    row.names(sobject_exprs_symbol) <- sobject_exprs_symbol$SYMBOL
+
+    sobject_exprs_symbol <- sobject_exprs_symbol[,-which(colnames(sobject_exprs_symbol) %in% 'SYMBOL')] %>% as.matrix()
+
+    simpleMultiAssay <- c(sobject,assay_reduce=sobject_exprs_symbol)
+    return(simpleMultiAssay)
+  }
+
 
 }
 
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -46,10 +46,31 @@ add_new_data(edit_files)
 
 #----------------------------------------------------
 # Add 4 data on Jan. 31st
-add_files <- paste0(c("GSE101705_sobject","GSE112104_sobject",
-                       "GSE107104_sobject","GSE89403_sobject"),".RDS")
-add_new_data(add_files)
+# add_files <- paste0(c("GSE101705_sobject","GSE112104_sobject",
+#                       "GSE107104_sobject","GSE89403_sobject"),".RDS")
+# add_new_data(add_files)
 ## Test
-tt <- lapply(add_files, function(x) readRDS(paste0("~/Desktop/curatedTBData/data-raw/",x)))
-tt_multi <- lapply(tt, function(x) Create_MultiAssay_object(x))
+# tt <- lapply(add_files, function(x) readRDS(paste0("~/Desktop/curatedTBData/data-raw/",x)))
+# tt_multi <- lapply(tt, function(x) Create_MultiAssay_object(x))
 
+# Modify RNA-seq data on Feb. 22th
+add_new_data_mobject <- function(edit_files){
+  library(purrr)
+  library(devtools)
+  sobject_total <- lapply(edit_files,
+                          function(x) readRDS(paste0("data-raw/",x)))
+  # sobject_total_name_ori <- lapply(sobject_total, function(x) names(assays(x)))
+  # sobject_total_name <- lapply(sobject_total_name_ori, function(x) gsub("(.+?)(\\_.*)", "\\1", x)) %>% unlist()
+  # sobject_total_name
+  names(sobject_total) <- gsub("\\..*","",edit_files)
+
+  purrr::walk2(sobject_total, paste0(names(sobject_total)), function(obj, name) {
+    assign(name, obj)
+    do.call("use_data", list(as.name(name), compress = "xz", overwrite = TRUE))
+  })
+
+}
+edit_files <- paste0(c("GSE101705_mobject", "GSE107104_mobject", "GSE89403_mobject",
+                      "GSE79362_mobject","GSE94438_mobject", "GSE107991_mobject",
+                      "GSE107992_mobject","GSE107993_mobject","GSE107994_mobject"),".RDS")
+add_new_data_mobject(edit_files)
