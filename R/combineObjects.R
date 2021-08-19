32b54ad0256e1ce572b5a57908b5354d09ad9dc2
xutao-wang <xutao.wang16@gmail.com>
2021-08-18 23:34:18 -0400

    UpdateDESCRIPTION & functions
    Former-commit-id: b949321c86985bcbf4622840754e2795beb96b65
    Former-commit-id: 7f92bd274aaf4cff404582ae92ac1094c80a47fe [formerly f18422b7d5281e21afde56afcdfa65d959aa3616] [formerly a20ef8e96246229ab3d15a67b9321bcabc87d4de [formerly 111949aa6b0dff92a15967849ec3d15b93ffe69b]]
    Former-commit-id: 25e47fef0647b406a0fc537a3773d91201ed3d11 [formerly 5be85d34c44c3df17d22535e8aeeafbda3d6673a]
    Former-commit-id: 5c84904ddfc561f508e22537ba05897fb9ec59c6

---
+++ R/combineObjects.R
@@ -0,0 +1,97 @@
+#' Merge samples with common genes from selected studies
+#' @name combineObjects
+#' @param object_list A `list` of [MultiAssayExperiment][MultiAssayExperiment::MultiAssayExperiment-class] objects.
+#' The assays contains object's assay contain expression data with probes mapped to gene symbol.
+#' `names(object_list)` should NOT be `NULL`.
+#' @param experiment_name A character/vector of character to choose the name of the assay from
+#' [MultiAssayExperiment][MultiAssayExperiment::MultiAssayExperiment-class] object.
+#' @return A [SummarizedExperiment][SummarizedExperiment::SummarizedExperiment-class]
+#' object contains combined data from the input.
+#' @examples
+#' geo <-  c("GSE19435", "GSE19439")
+#' data_list <-  curatedTBData(geo, dryrun = FALSE, curated.only = TRUE)
+#' sobject <- combineObjects(data_list, experiment_name = "assay_curated")
+#' @export
+combineObjects <- function(object_list, experiment_name = NULL) {
+  # check length of the list, should be greater than 1
+  if (base::length(object_list) <= 1) {
+    base::stop("The length of the input list is %s, expected be larger than 1 for combining objectss.",
+               base::length(object_list))
+  }
+  # check the experiment_name argument
+  if (base::is.null(experiment_name)) {
+    base::stop("Missing experiment name for the MultiAssayExperiment object")
+  }
+  # check names of the input object list
+  obj_name <- base::names(object_list)
+  if (base::is.null(obj_name)) {
+    base::stop("Names of the input list should not be NULL. Add unique names for each object within the list.")
+  } else if (!base::is.na(base::match("", obj_name))) {
+    base::stop("Names of the input contains \"\". Replace \"\" with a non-empty character.")
+  }
+  if (base::length(experiment_name) > 1) {
+    # experiment name for the list of object is different
+    if (base::length(experiment_name) == base::length(object_list)) {
+      dat_exprs_match <- base::mapply(function(x, y) {
+        MultiAssayExperiment::experiments(x)[[y]] %>%
+          base::as.data.frame()
+      }, object_list, experiment_name)
+    } else {
+      message("More than one experiment_name observed.")
+      base::stop("The length of input list is not the same as the length of the experiment name vector.")
+    }
+  } else {
+    dat_exprs_match <- base::lapply(object_list, function(x)
+      MultiAssayExperiment::experiments(x)[[experiment_name]] %>%
+        base::as.data.frame())
+  }
+  # Combine sample with common genes from a list of objects.
+  # Input data type should be data.frame
+  dat_exprs_combine <- base::Reduce(function(x, y)
+    base::merge(x, y, by = "id", all = FALSE),
+    base::lapply(dat_exprs_match, function(x) {
+      x$id <- base::row.names(x)
+      x
+    })
+  )
+  row_names <- dat_exprs_combine$id
+  dat_exprs_count <- dat_exprs_combine %>%
+    dplyr::select(-.data$id) %>%
+    base::as.data.frame()
+  base::row.names(dat_exprs_count) <- row_names
+
+  # Create combined column data information
+  Sample1 <- base::lapply(object_list, function(x)
+    SummarizedExperiment::colData(x) %>%
+      base::row.names())
+
+  Sample <- base::unlist(Sample1, use.names = FALSE)
+  col_data <- base::lapply(base::seq_len(base::length(object_list)), function(x) {
+    col_data <- SummarizedExperiment::colData(object_list[[x]])
+    col_data$Study <- base::names(object_list[x])
+    base::as.data.frame(col_data)
+  })
+
+  # Combine list into data frame with unequal columns, fill in NA
+  # when columns from studies are not found
+  rbindx <- function(dfs) {
+    ns <- base::unique(base::unlist(base::sapply(dfs, base::colnames)))
+    base::do.call(base::rbind, base::lapply(dfs, function(x) {
+      for (n in ns[!ns %in% base::colnames(x)]) {
+        x[[n]] <- NA
+      }
+      x
+    }
+    ))
+  }
+  col_info <- rbindx(col_data)
+  base::row.names(col_info) <- Sample
+  # Remove samples that does not exist in the count
+  index <- stats::na.omit(base::match(base::colnames(dat_exprs_count), Sample))
+  col_info <- col_info[index, ]
+  # Create output in the format of SummarizedExperiment
+  result <- SummarizedExperiment::SummarizedExperiment(
+    assays = base::list(assay1 = as.matrix(dat_exprs_count)),
+    colData = col_info)
+  return(result)
+}
